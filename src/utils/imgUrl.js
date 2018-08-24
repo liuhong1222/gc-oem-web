2d65753bedbf214885caa01b5a20ae6c37b10dec
moshujie <mosj@253.com>
2018-08-24 18:01:42 +0800

    '修改代理商新增'

--- src/utils/imgUrl.js
+++ src/utils/imgUrl.js
@@ -1,5 +1,6 @@
 const imgUrlConfig={
     imgUrl:''
 };
-imgUrlConfig.imgUrl = 'http://127.0.0.1:9191/';
+<!-- imgUrlConfig.imgUrl = 'http://127.0.0.1:9191/'; -->
+imgUrlConfig.imgUrl = 'http://shdb.253.com:8180';
 export default imgUrlConfig
